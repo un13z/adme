47b97c9b75b1040639595fc7595f6b10db487962
un13z <97453482+un13z@users.noreply.github.com>
2022-01-25 09:48:45 +0800

    Create admeall.js

---
+++ admeall.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+'https://backmerriment.com/xry4p4n5?key=c736c05c29a9fa181b16fa73677fdcd7',
+'https://backmerriment.com/m9m9fuhf0j?key=bd0ccfd9067257eccefaf7a1cbb7b90e',
+'https://backmerriment.com/xcj8mqd5y3?key=ab3490eb6b1631ba541762988045c098',
+'https://backmerriment.com/dy9q80j1?key=78a1ee0612a84af4a12b0a4c1318d7b5',
+'https://backmerriment.com/c0fi5rrc5?key=48bbee5b0df45e5e4a8a17524638c45b'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
